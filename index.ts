c1709a003ab5a0b1f2d513d6239dc526ccb2f995
utsarapkin <utsarapkin@fnc-group.ru>
2023-01-24 02:18:35 +0300

    feat: initial configuration

---
+++ index.ts
@@ -0,0 +1,13 @@
+import httpServer from './src/http_server/index.js';
+import webSocketServer from './src/webSocketServer/index.js';
+
+const HTTP_PORT = 3000;
+
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
+
+webSocketServer();
+
+process.on('uncaughtException', (err: Error) => {
+  console.error('uncaughtException', err);
+});
